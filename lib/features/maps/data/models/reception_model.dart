b17a2c1141ba5d5e869da9a010ef301b2ac32512
Kelly Bentes <kellymaria.bentes@gmail.com>
2020-07-12 17:10:53 -0300

    updates in models

--- lib/features/maps/data/models/reception_model.dart
+++ lib/features/maps/data/models/reception_model.dart
@@ -17,11 +17,9 @@ class ReceptionModel extends Reception {
     );
   }
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    if (weekDay != null) data['weekDay'] = this.weekDay;
-    if (weekDay != null) data['openingHour'] = this.openingHour;
-    if (weekDay != null) data['closingHour'] = this.closingHour;
-    return data;
+  static List<ReceptionModel> listFromJson(List<dynamic> json) {
+    return json == null
+        ? List<ReceptionModel>()
+        : json.map((value) => ReceptionModel.fromJson(value)).toList();
   }
 }
